bdf67fbfe23f4302487d212042cc4e66bfac83a7
yuu04rip <99676204+yuu04rip@users.noreply.github.com>
2026-08-10 21:18:26 +0200

    Fix bug

--- app/src/main/java/com/example/quester/ui/screens/NavBar.kt
+++ app/src/main/java/com/example/quester/ui/screens/NavBar.kt
@@ -4,11 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.ui.unit.dp
 import com.example.quester.data.dao.ShopDao
 import com.example.quester.data.repository.MissionRepository
